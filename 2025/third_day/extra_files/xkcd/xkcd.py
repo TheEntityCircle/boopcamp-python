6f7f5a46ae45c4c138f92976c23633c06295c433
Amisto <amisto@yandex.ru>
2025-08-29 08:55:18 +0300

    Additional point-pictures for the third day, also xkcd

---
+++ 2025/third_day/extra_files/xkcd/xkcd.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+plt.xkcd()
+x = []
+for i in range(100):
+    x.append(2 * i / 100)
+
+y1, y2, y3 = [], [], []
+for i in x:
+    y1.append(i)
+    y2.append(i**2)
+    y3.append(i**3)
+
+# А теперь создадим изображение,
+# на котором будет сразу несколько 
+# строк и столбцов
+fig, axs = plt.subplots(nrows=2, ncols=2)
+# Теперь в axs - двумерный массив "осей", в которых мы построим свои y(x) по отдельности
+axs[0][0].plot(x, y1, label='linear')
+axs[1][0].plot(x, y2, label='quadratic')
+axs[1][1].plot(x, y3, label='cubic')
+
+axs[0][0].grid(True)
+axs[1][0].grid(True)
+axs[1][1].grid(True)
+
+# Всё изображение целиком тоже подпишем
+fig.suptitle("Several subplots", size=18)
+plt.legend()
+plt.savefig('xkcd.png')
+plt.show()
